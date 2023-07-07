8bf8e508b893f7f8af39bda67dd08b0f5f6dba01
Yavuz Ulgar <digitalworksyavuz@gmail.com>
2023-07-07 17:44:46 +0300

    UI Ready

---
+++ HesapUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HesapUI
+//
+//  Created by Yavuz Ulgar on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
